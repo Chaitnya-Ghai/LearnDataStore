2303eee2f95e914ab7ed684c8a396226082390b8
Chaitnya-Ghai <chaitnyaghai2004@gmail.com>
2025-08-30 21:31:27 +0530

    Refactor: Add DataStore functionality in MainActivity.kt
    This commit refactors the `MainActivity` to include:
    - A `MainScreen` composable with TextFields for key-value input.
    - Buttons to save and retrieve data using `PreferencesDataStore`.
    - Display of stored information.
    - Removal of the `Greeting` composable and its preview.

--- app/src/main/java/com/example/learndatastore/MainActivity.kt
+++ app/src/main/java/com/example/learndatastore/MainActivity.kt
@@ -4,26 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.learndatastore.ui.theme.LearnDataStoreTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val preferencesDataStore by lazy { PrefUtils(this) }
         setContent {
             LearnDataStoreTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(modifier = Modifier.padding(innerPadding),preferencesDataStore)
                 }
             }
         }
@@ -31,17 +44,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    preferencesDataStore: PrefUtils
+) {
+    val key = remember { mutableStateOf("") }
+    val value = remember { mutableStateOf("") }
+    val storedInfo = remember { mutableStateOf("") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LearnDataStoreTheme {
-        Greeting("Android")
+    val scope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(value = key.value, onValueChange = {
+            key.value = it
+        }, modifier = Modifier.fillMaxWidth())
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        TextField(value = value.value, onValueChange = {
+            value.value = it
+        }, modifier = Modifier.fillMaxWidth())
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(text = storedInfo.value, style = MaterialTheme.typography.headlineLarge)
+
+        Button(onClick = {
+            scope.launch(Dispatchers.IO) {
+                preferencesDataStore.saveString(key.value, value.value)
+            }
+        }) {
+            Text("Save")
+        }
+
+        Button(onClick = {
+            scope.launch(Dispatchers.IO) {
+                storedInfo.value = preferencesDataStore.getString(key.value).toString()
+            }
+        }) {
+            Text(text = "Get info")
+        }
     }
 }
